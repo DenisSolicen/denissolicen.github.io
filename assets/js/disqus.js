5e3920a1272e04b10590551b9685d88ca9df511b
DenisSolicen <DenisSolicen@yandex.ru>
2022-12-25 20:41:27 +0300

    Рефакторинг

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -19,20 +19,27 @@ String.prototype.endsWith = function (s) {
     $(function () {
         var key = "SyRBH30uqdyxbqzpRcJ96FZFqXP0GjKs3calfHDo4Ln5Gu5i29IfV3CJIFIqPcVC";
         var forum = "solicen";
+        var urls = [];
+
         $('.count-comments').each(function (e) {
             var url = $(this).attr('data-disqus-url');
-            console.log(url);
-            var el = document.getElementById(url);
-            var disqusUrl = 'https://disqus.com/api/3.0/forums/listThreads.json?forum='+encodeURIComponent(forum)+'&api_key='+key+'&limit=100';
-            $.get(disqusUrl).then(function(res) {
-              res.response.forEach(function(t) {  
-                if (t.posts != -1){
-                  console.log(t.clean_title + " | комментарии: " + t.posts + "\n");                     
+            urls.push(url);
+        });
+
+        $(function () {
+          var disqusUrl = 'https://disqus.com/api/3.0/forums/listThreads.json?forum='+encodeURIComponent(forum)+'&api_key='+key+'&limit=100';
+          $.get(disqusUrl).then(function(res) {
+            res.response.forEach(function(t) {  
+              if (t.posts != -1){
+                urls.forEach(url => {
+                  var el = document.getElementById(url);
                   if (t.link.endsWith(url)){
-                      el.innerHTML = t.posts;
-                  }                      
-                } 
-              })
-            },'json');
+                    el.innerHTML = t.posts;
+                    //console.log(t.clean_title + " | комментарии: " + t.posts + "\n");   
+                }  
+                });                                  
+              } 
+            })
+          },'json');
         });
     });
